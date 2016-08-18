3c44477e801d3114792a867b0784331eda12241a
AIT AKKACHE Soufiane <saitakkache@agb.dz>
2016-08-18 16:33:11 +0100

    third commit

---
+++ src/components/Datepicker.vue
@@ -0,0 +1,44 @@
+<style>
+  .datepicker_container {
+    position: relative;
+  }
+</style>
+<template>
+  <div class="datepicker_container">
+    <input type="text" value="{{ date_formatted }}">
+    <input type="text" name="{{ name }}" value="{{ date_raw }}">
+    <datepicker-agenda v-bind:date="date"></datepicker-agenda>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+import DatePickerAgendaComponent from './datepickerAgenda'
+
+moment.locale('fr')
+
+export default {
+  components: {
+    'datepicker-agenda': DatePickerAgendaComponent
+  },
+  props: {
+    value: {type: String, required: true},
+    format: {type: String, default: 'YYYY/MM/DD'},
+    name: {type: String}
+  },
+  data: function () {
+    return {
+      date: moment(this.value, 'YYYY-MM-DD')
+    }
+  },
+  computed: {
+    date_formatted: function () {
+      return this.date.format(this.format)
+    },
+    date_raw: function () {
+      return this.date.format('YYYY-MM-DD')
+    }
+  }
+}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-
-import datepickerComponent from './components/Datepicker'
-
+import Vue from 'vue'
+import DatepickerComponent from './components/Datepicker'
 /* eslint-disable no-new */
-/* global _ */
 new Vue({
   el: 'body',
   components: {
-    datepicker: datepickerComponent
+    datepicker: DatepickerComponent
   }
 })
